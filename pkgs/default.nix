3370f5835558a13ef7ced19180fcf90a52180fcc
Chen <i@cuichen.cc>
2024-03-24 01:40:10 +0000

    clean unused pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,4 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 { pkgs, ... }: {
-  ott-sweirich = pkgs.callPackage ./ott-sweirich.nix {
-    ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_12;
-  };
 }
